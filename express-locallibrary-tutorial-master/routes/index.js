7ad9c83d0f7ad0f9b76bdafdb681f4ae9b234da8
sumit391 <sumitatbits@gmail.com>
2017-03-18 11:03:00 +0100

    added db data with parameters

--- express-locallibrary-tutorial-master/routes/index.js
+++ express-locallibrary-tutorial-master/routes/index.js
@@ -12,12 +12,18 @@ router.get('/', function(req, res) {
   //if (res.param('code')){
   //	res.send('the access code is :' + res.param('code'));
   //}
-  var token;
-  res.redirect('https://simulator-api.db.com/gw/oidc/authorize?response_type=code&client_id=24b8e99d-4c0c-4850-a069-169c6f5e810a&state=https://starthack2017.herokuapp.com/');
+  //var token;
+  res.redirect('https://simulator-api.db.com/gw/oidc/authorize?response_type=code&client_id=24b8e99d-4c0c-4850-a069-169c6f5e810a&state=https://starthack2017.herokuapp.com/algo');
   //res.send(token);
-  token = res.code;
-  res.send(token);
+  //token = res.code;
+  //res.send(token);
   //res.status(200).send('Done till here');
+});
+router.get('/algo', function(req, res) {
+  res.send('Add Algorithm here');
+  //res.redirect('/catalog');
+  //res.redirect('http://google.com');
+  //res.redirect('https://simulator-api.db.com/gw/oidc/authorize?response_type=code&client_id=24b8e99d-4c0c-4850-a069-169c6f5e810a&state=https://starthack2017.herokuapp.com/algo');
 });
 
 //router.get('/https://simulator-api.db.com/gw/oidc/authorize', function(req, res)){
