ec257792836bfab12b9eda080d3c759cadb24a01
sumit391 <sumitatbits@gmail.com>
2017-03-18 07:01:11 +0100

    added google.com

--- express-locallibrary-tutorial-master/routes/index.js
+++ express-locallibrary-tutorial-master/routes/index.js
@@ -3,8 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.send('Add DB Data here');
-  res.redirect('/catalog');
+  //res.send('Add DB Data here');
+  //res.redirect('/catalog');
+  res.redirect('http://google.com');
 });
 
 module.exports = router;
