8b8edb6c478a9a7fabafbbaf9736d9e94be64b60
sumit391 <sumitatbits@gmail.com>
2017-03-18 11:55:28 +0100

    added algo.js code

--- express-locallibrary-tutorial-master/routes/index.js
+++ express-locallibrary-tutorial-master/routes/index.js
@@ -19,12 +19,14 @@ router.get('/', function(req, res) {
   //res.send(token);
   //res.status(200).send('Done till here');
 });
-router.get('https://simulator-api.db.com/gw/oidc/authorize', function(req, res) {
-  res.send('Add Algorithm here');
+
+//router.get('https://simulator-api.db.com/gw/oidc/authorize?response_type=code&client_id=24b8e99d-4c0c-4850-a069-169c6f5e810a&state=https://starthack2017.herokuapp.com/')
+//router.get('https://simulator-api.db.com/gw/oidc/authorize', function(req, res) {
+//  res.send('Add Algorithm here');
   //res.redirect('/catalog');
   //res.redirect('http://google.com');
   //res.redirect('https://simulator-api.db.com/gw/oidc/authorize?response_type=code&client_id=24b8e99d-4c0c-4850-a069-169c6f5e810a&state=https://starthack2017.herokuapp.com/algo');
-});
+//});
 
 //router.get('/https://simulator-api.db.com/gw/oidc/authorize', function(req, res)){
 //  res.status(200).send('Done till here');	
