575b7bf147315208c110f212d7da18db1a3a00b4
sumit391 <sumitatbits@gmail.com>
2017-03-18 10:51:14 +0100

    added google.com

--- express-locallibrary-tutorial-master/routes/index.js
+++ express-locallibrary-tutorial-master/routes/index.js
@@ -15,7 +15,7 @@ router.get('/', function(req, res) {
   var token;
   res.redirect('https://simulator-api.db.com/gw/oidc/authorize?response_type=code&client_id=24b8e99d-4c0c-4850-a069-169c6f5e810a&state=https://starthack2017.herokuapp.com/');
   //res.send(token);
-  token = res.code;
+  //token = res.code;
   //res.status(200).send('Done till here');
 });
 
