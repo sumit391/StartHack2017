8d8a4159bcbb20aa9392d31ce515af36041ed26d
sumit391 <sumitatbits@gmail.com>
2017-03-18 07:07:22 +0100

    added db data with parameters

--- express-locallibrary-tutorial-master/routes/index.js
+++ express-locallibrary-tutorial-master/routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res) {
   //res.send('Add DB Data here');
   //res.redirect('/catalog');
   //res.redirect('http://google.com');
-  res.redirect('https://simulator-api.db.com/gw/oidc/authorize');
+  res.redirect('https://simulator-api.db.com/gw/oidc/authorize?response_type=code&client_id=24b8e99d-4c0c-4850-a069-169c6f5e810a&state=/');
 });
 
 module.exports = router;
