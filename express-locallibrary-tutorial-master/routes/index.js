cf4489f4f8d1a746e2e66a14c0805d33475b29dd
sumit391 <sumitatbits@gmail.com>
2017-03-18 10:26:21 +0100

    added status message for sucesful return

--- express-locallibrary-tutorial-master/routes/index.js
+++ express-locallibrary-tutorial-master/routes/index.js
@@ -8,10 +8,17 @@ router.get('/', function(req, res) {
   //res.redirect('http://google.com');
 
   // Check for the code here
-  if (res.param('code')){
-  	res.send('the access code is :' + res.param('code'));
-  }
-  res.redirect('https://simulator-api.db.com/gw/oidc/authorize?response_type=code&client_id=24b8e99d-4c0c-4850-a069-169c6f5e810a&state=https://starthack2017.herokuapp.com/catalog/books');
+  //res.param
+  //if (res.param('code')){
+  //	res.send('the access code is :' + res.param('code'));
+  //}
+  res.redirect('https://simulator-api.db.com/gw/oidc/authorize?response_type=code&client_id=24b8e99d-4c0c-4850-a069-169c6f5e810a&state=https://starthack2017.herokuapp.com/');
 });
 
+router.get('/https://simulator-api.db.com/gw/oidc/authorize', function(req, res)){
+  res.status(200).send('Done till here');	
+  //res.status(200).post('https://simulator-api.db.com/gw/oidc/token')
+};
+
+//router.post('https://simulator-api.db.com/gw/oidc/token')
 module.exports = router;
